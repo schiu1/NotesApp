391d0f2118bd2d0e163e2dc684d77c9f634a4a2c
Stephen <gstephenc@hotmail.com>
2023-11-16 18:29:16 -0500

    trying to push most recently updated to top

--- notesApp.js
+++ notesApp.js
@@ -2,7 +2,16 @@ const plus = document.getElementById("plus-icon");
 plus.addEventListener("click", AddNote);
 
 function AddNote(){
-    const original = document.getElementById("note1");
-    const clone = original.cloneNode(true);
+    const original = document.getElementById("note2"); //change this to be more general
+    const allNotes = document.getElementsByClassName("note");
+    let clone = original.cloneNode(true);
+    clone.id = "note" + (allNotes.length + 1);
     document.body.appendChild(clone);
+    for(let i = 0; i < allNotes.length; i++){
+        console.log(allNotes[i]);
+        document.body.appendChild(allNotes[i]);
+        //supposed to get note1 and note2 and append them to the end of note3, pushing the newest created note to the top
+    }
+    
+    
 }
