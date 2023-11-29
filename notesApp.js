d0ed09c246893cc09e00e3abffc3b5346164e5ba
Stephen <gstephenc@hotmail.com>
2023-11-29 14:36:27 -0500

    AddNote now creates note from scratch instead of cloning existing one
    - need to add confirmation alert to DeleteNote
    - need to add hover + onclick style to trash icon and plus icon
    - need to add update function with time record
    - need to fix style with header when resizing window

--- notesApp.js
+++ notesApp.js
@@ -3,28 +3,41 @@ plus.addEventListener("click", AddNote);
 let noteCount = 1;
 
 function AddNote(){
-    //add new note with unique id, which will append to end
+    /*
     const allNotes = document.getElementsByClassName("note");
     const original = allNotes[0];
     let clone = original.cloneNode(true);
-    clone.id = "note" + (noteCount + 1);
-    document.body.appendChild(clone);
+    */
+    //create new note with unique id, which will append to end
+    const trashIcon = document.createElement("i"); 
+    trashIcon.classList.toggle("fa");
+    trashIcon.classList.toggle("fa-trash");
+    trashIcon.id = "trash-icon";
+    trashIcon.setAttribute("aria-hidden", "true");
+
+    const textSpace = document.createElement("textarea");
+    textSpace.classList.toggle("noteText");
+    textSpace.id = "noteText";
+    
+    const createdNote = document.createElement("div");
+    createdNote.classList.toggle("note");
+    createdNote.id = "note" + (noteCount + 1);
+
+    createdNote.appendChild(trashIcon);
+    createdNote.appendChild(textSpace);
+    document.body.appendChild(createdNote);
     noteCount++;
 
     //make changes for it to perform as intended
-    const newNote = document.getElementById(clone.id);
-    console.log("innerHTML: " + newNote.getElementsByTagName("textarea")[0].innerHTML);
-    console.log("value: " + newNote.getElementsByTagName("textarea")[0].value);
-    const textarea = newNote.getElementsByTagName("textarea")[0];
-    textarea.innerHTML = "";
-    textarea.value = "";
+    const newNote = document.getElementById(createdNote.id);
     const deleteIcon = newNote.getElementsByTagName("i")[0];
-    deleteIcon.onclick = function(){DeleteNote(clone.id)}; 
+    deleteIcon.onclick = function(){DeleteNote(createdNote.id)}; 
 
     //append all other notes behind newest created, pushing it to top
     //do the same with updating a note but add if statemnt to check if same name as updated note
     //if it is, continue
     let counter = 0;
+    const allNotes = document.getElementsByClassName("note");
     while(counter < allNotes.length - 1){
         document.body.appendChild(allNotes[0]);
         counter += 1;
@@ -32,7 +45,6 @@ function AddNote(){
 }
 
 function DeleteNote(noteNum){
-    console.log(noteNum);
     const elem = document.getElementById(noteNum);
     elem.remove();
 }
