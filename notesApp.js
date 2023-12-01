1816d0eaf6b9e21ad9a5f527018714692d6b525d
Stephen <gstephenc@hotmail.com>
2023-11-30 19:39:14 -0500

    removed markdown features for now

--- notesApp.js
+++ notesApp.js
@@ -1,5 +1,5 @@
-import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
-document.getElementById('noteText').innerHTML = marked.parse('# Marked in the browser\n\nRendered by **marked**.');
+//import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+//document.getElementById('noteText').innerHTML = marked.parse('# Marked in the browser\n\nRendered by **marked**.');
 const plus = document.getElementById("plus-icon");
 plus.addEventListener("click", AddNote);
 let noteCount = 1;
